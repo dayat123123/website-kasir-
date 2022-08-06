30be004a26cd9841a07ec9aeffc8621e51b0f21e
dayat123123 <dayatburgerkill389@gmail.com>
2022-08-06 18:28:38 +0700

    bikin fitur tampilkan berdasarkan tanggal

--- admin/transaksi.php
+++ admin/transaksi.php
@@ -21,13 +21,55 @@ include '../koneksi.php';
         <div class="box box-info">
           <div class="box-header">
           <h3 class="box-title">Data Transaksi</h3>
+          <div class="box-body">
+            <form method="get" action="">
+              <div class="row">
+                <div class="col-md-3">
+                  <div class="form-group">
+                    <label>Berdasarkan Kasir</label>
+                    <select name="kasir" class="form-control" required="required">
+                      <option value="semua">- Semua kasir -</option>
+                      <?php 
+                      $kategori = mysqli_query($koneksi,"SELECT * FROM users where user_level = 'kasir'");
+                      while($k = mysqli_fetch_array($kategori)){
+                        ?>
+                        <option <?php if(isset($_GET['nama'])){ if($_GET['nama'] == $k['id']){echo "selected='selected'";}} ?>  value="<?php echo $k['id']; ?>"><?php echo $k['nama']; ?></option>
+                        <?php 
+                      }
+                      ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                    <label>Mulai Tanggal</label>
+                    <input autocomplete="off" type="text" value="<?php if(isset($_GET['tanggal_dari'])){echo $_GET['tanggal_dari'];}else{echo "";} ?>" name="tanggal_dari" class="form-control datepicker2" placeholder="Mulai Tanggal" required="required">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                    <label>Sampai Tanggal</label>
+                    <input autocomplete="off" type="text" value="<?php if(isset($_GET['tanggal_sampai'])){echo $_GET['tanggal_sampai'];}else{echo "";} ?>" name="tanggal_sampai" class="form-control datepicker2" placeholder="Sampai Tanggal" required="required">
+                    </div>
+                    </div>
+                <div class="col-md-3">
+
+                  <div class="form-group">
+                    <br/>
+                    <input type="submit" value="TAMPILKAN" class="btn btn-sm btn-primary btn-block">
+                  </div>
+
+                </div>
+              </div>
+            </form>
+            
             <div class="btn-group pull-right">            
             </div>
           </div>
           <div class="box-body">
 
             <div class="table-responsive">
-              <table class="table table-bordered table-striped" id="table-datatable">
+              <table class="table table-bordered table-striped" >
                 <thead>
                   <tr>
                     <th width="2%">ID</th>
